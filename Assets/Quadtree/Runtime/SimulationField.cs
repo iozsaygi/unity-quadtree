64663913a37d597cbef0c9962b5d0c2980ea3771
İsmail Özsaygı <devozsaygi@gmail.com>
2024-06-17 20:53:20 +0300

    Generating simulation entities inside field

--- Assets/Quadtree/Runtime/SimulationField.cs
+++ Assets/Quadtree/Runtime/SimulationField.cs
@@ -9,12 +9,30 @@ namespace Quadtree.Runtime
     public class SimulationField : MonoBehaviour
     {
         [SerializeField] private Bounds bounds;
+        [SerializeField] private GameObject simulationEntityPrefab;
+        [SerializeField] private byte maximumNumberOfAllowedSimulationEntities;
+
+        private void Start()
+        {
+            // Generate simulation entity prefabs at random positions within simulation bounds.
+            for (byte i = 0; i < maximumNumberOfAllowedSimulationEntities; i++)
+            {
+                // Calculate random position within simulation bounds.
+                var randomPositionWithinSimulationBounds = new Vector3(Random.Range(bounds.min.x, bounds.max.x),
+                    Random.Range(bounds.min.y, bounds.max.y), 0.0f);
+
+                var simulationEntityInstance = Instantiate(simulationEntityPrefab, randomPositionWithinSimulationBounds,
+                    Quaternion.identity);
+
+                simulationEntityInstance.transform.SetParent(transform, true);
+            }
+        }
 
         private void OnDrawGizmosSelected()
         {
             // Rendering the extents of the simulation field.
             Gizmos.color = Color.white;
-            Gizmos.DrawWireCube(bounds.center, bounds.extents);
+            Gizmos.DrawWireCube(bounds.center, bounds.size);
         }
     }
 }
